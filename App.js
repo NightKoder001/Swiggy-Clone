29075bcb9775502484d5f14ba29125592364cde4
Nightcoder001 <gautamj.iitm@gmail.com>
2023-09-14 06:10:53 +0530

    first push

--- App.js
+++ App.js
@@ -1,19 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id:"child1"},[
-        React.createElement("h1", {}, "I'm a h1 tag of child1"),
-        React.createElement("h2", {}, "I'm a h2 tag of child1")
-    ] ),
-    React.createElement("div", {id:"child2"},[
-        React.createElement("h1", {}, "I'm a h1 tag of child2"),
-        React.createElement("h2", {}, "I'm a h2 tag of child2")
-    ] )
-]
-
-)
-
-
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);
+const heading = React.createElement('h1', {id: "heading", className: "abc"}, "Hello World from React");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
